74b56d656bb93c5f513650ab7fd5a836c5ae89f5
Ralphd42 <rdonato@albany.edu>
2017-03-04 22:11:25 -0500

    Added main to start testing

---
+++ HW3.java
@@ -0,0 +1,14 @@
+
+public class HW3 {
+
+	public HW3() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DoubleLinkedSortedList dlsl = new DoubleLinkedSortedList();
+		Node nd = new Node("A111");
+	}
+
+}
