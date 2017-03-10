be75fd8d301698d24c010fbf103451da02bbab0b
Ralphd42 <rdonato@albany.edu>
2017-03-10 11:13:23 -0500

    Added list reverse

--- DoubleLinkedSortedList.java
+++ DoubleLinkedSortedList.java
@@ -1,5 +1,10 @@
 /**
- * 
+ * • Insert a node in the list in alphabetical order
+• Find a node that matches a String
+• Traverse the list forwards and print
+• Traverse the list backwards and print
+• Delete a node from the list
+• Delete/destroy the list
  */
 
 /**
@@ -76,7 +81,37 @@ public class DoubleLinkedSortedList
                 }
                 	
         }
-
+        public Node FindNode(String NodeText)
+        {
+        	Node tmp = first;
+            while (tmp!=null  && NodeText.compareTo(tmp.getDataItem()) !=0)
+            {
+            	tmp = tmp.getNext();
+            }
+        	return tmp;
+        }
+        
+        public boolean RemoveNode(String NodeText)
+        {
+        	boolean deleted = false;
+        	Node tmp = first;
+            while (tmp!=null  && NodeText.compareTo(tmp.getDataItem()) !=0)
+            {
+            	tmp = tmp.getNext();
+            }
+            if(tmp!=null)
+            {
+            	//Deelete the node
+            	Node Prev = tmp.getPrev();
+            	Node Next = tmp.getNext();
+            	// Set next of Prev to Next
+            	Prev.SetNext(Next);
+            	//Set Prev ofNext to 
+            	
+            }
+            
+        	return deleted;
+        }
 
 
 
